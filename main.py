2dbfe8e44746eeb2765e4053741b1def47869c9d
elisar.gstat <Q!Q!w2w2>
2020-09-03 11:05:24 +0000

    new feature + keras

--- main.py
+++ main.py
@@ -1,22 +1,9 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+tqdm.pandas()
 
 
 def transform_raw_data(data):
@@ -69,7 +56,7 @@ def transform_data(data):
 
 def code_des(df, lim):
     status_description_dict = {
-        0: 'off',  ## on
+        0: 'off',
         1: 'energy_consuming',
         2: 'compressing'
     }
@@ -78,7 +65,6 @@ def code_des(df, lim):
         {'ts': ['min', 'max', 'count'], 'was_na_mean': 'mean', 'shutdown_mal_code': 'mean'})
     info[('', 'high malfunctions')] = 0
     info.loc[info[('shutdown_mal_code', 'mean')] > lim, ('', 'high malfunctions')] = 1
-
     high_malfunctions = info.loc[info[('', 'high malfunctions')] == 1, :].index
     df = df.loc[~df.index.get_level_values(0).isin(high_malfunctions), :]
     return df, info
@@ -95,8 +81,7 @@ def resample_period(df, period):
     return pp
 
 
-def shorten_label(df1):
-    df_tmp = df1.copy()
+def shorten_label(df_tmp):
     tmp = df_tmp[['shutdown_mal_code']].query('shutdown_mal_code==1').reset_index()
     df_tmp.drop('shutdown_mal_code', axis=1, inplace=True)
     first_ocurence = tmp['ts'].diff().astype('timedelta64[D]').fillna(0)
@@ -114,13 +99,49 @@ def minimum_timespan(df, period):
     minimal_time_span = minimal_time_span.rename('ts_end').to_frame().reset_index()
     minimal_time_span['ts'] = minimal_time_span['ts'] + pd.Timedelta(minutes=1)
 
-    tmp = minimal_time_span.apply(lambda x: pd.date_range(start=x['ts'], end=x['ts_end'], freq='H'), axis=1)
+    tmp = minimal_time_span.apply(lambda x: pd.date_range(start=x['ts'], end=x['ts_end'], freq='min'), axis=1)
     tmp.index = minimal_time_span['device_id'].values  #
     period_to_na = tmp.explode().to_frame().reset_index()
     period_to_na.columns = ['device_id', 'ts']
     period_to_na = period_to_na.set_index(['device_id', 'ts'])
     period_to_na['shutdown_mal_code'] = 2
     df.update(period_to_na)
+
+
+def from_time_series(df, n_in, join =True):
+    reframed = df.query('shutdown_mal_code != 2')[['compressor_status_code']].copy()
+    reframed = series_to_supervised(reframed, n_in, 0)
+    reframed = reframed.div(2)
+    if join:
+        reframed = reframed.join(df['shutdown_mal_code'], how='left')
+        print(reframed['shutdown_mal_code'].value_counts())
+    return reframed
+
+
+def string_batch(df_tmp, size):
+    agg = df_tmp.astype('int').astype('str').copy()
+    cnt_columns = agg.shape[1]
+    mod = cnt_columns % size
+    cols = list()
+    for i in tqdm(range(0, cnt_columns - 1 - mod, size), desc='columns batch'):  #
+        col = agg.iloc[:, i:i + size].apply(lambda x: ''.join(x), axis=1)  # progress_apply
+        cols.append(col)
+
+    if 0 < mod:
+        tqdm.pandas(desc="my bar!2")
+        col = pad_sequences(agg.iloc[:, -mod:].values.tolist(), maxlen=size, padding='post')
+        cols.append(col.progress_apply(lambda x: ''.join(x), axis=1))
+
+    new_df = pd.concat(cols, axis=1)
+    return new_df
+
+
+def remove_rows(tmp, size):
+    tmp['row_num'] = tmp.groupby('device_id').cumcount()
+    tmp['mod'] = tmp['row_num'] % size
+    tmp.query('mod==0', inplace=True)
+    tmp.drop(['mod', 'row_num'], axis=1, inplace=True)
+    # return tmp
 
 
 def series_to_supervised(data, n_in=1, n_out=1, dropna=True):
@@ -145,13 +166,3 @@ def series_to_supervised(data, n_in=1, n_out=1, dropna=True):
     if dropna:
         agg.dropna(inplace=True)
     return agg
-
-
-# check version
-import tensorflow as tf
-
-print(tf.__version__)
-
-# example of a model defined with the sequential api
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense
